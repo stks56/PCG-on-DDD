eaa273111059271510b6588e1a2ca476dc048d3f
stks56 <t.suenaga428@gmail.com>
2021-11-18 05:22:21 +0900

    implement GetID() in repository

--- app/infrastructure/repository/cardRepository.go
+++ app/infrastructure/repository/cardRepository.go
@@ -3,16 +3,29 @@ package repository
 import (
 	"stks56/PCG-on-DDD/app/domain/model"
 	"stks56/PCG-on-DDD/app/domain/repository"
+	"stks56/PCG-on-DDD/app/infrastructure"
+	"stks56/PCG-on-DDD/app/infrastructure/dto"
+
+	"gorm.io/gorm"
 )
 
-type cardRepository struct{}
-
-func NewCardRepository() repository.CardRepository {
-	return &cardRepository{}
+type cardRepository struct {
+	db *gorm.DB
 }
 
-func (c *cardRepository) GetByID(id int) (*model.Card, error) {
-	// tmp
-	card := model.NewCard(id, "foo")
-	return card, nil
+func NewCardRepository() repository.CardRepository {
+	return &cardRepository{
+		db: infrastructure.GetDB(),
+	}
+}
+
+func (cr *cardRepository) GetByID(id int) (*model.Card, error) {
+	dto := dto.Card{}
+	cr.db.Where("id = ?", id).First(&dto)
+
+	card := model.Card{
+		Id:   id,
+		Name: dto.Name,
+	}
+	return &card, nil
 }
