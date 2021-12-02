3dc574931f85f6719f38d8c525fcdf0cd4e71ded
stks56 <t.suenaga428@gmail.com>
2021-12-03 05:37:17 +0900

    Fix incremental id

--- app/domain/model/deck.go
+++ app/domain/model/deck.go
@@ -7,7 +7,7 @@ type Deck struct {
 func NewPresetDeck() *Deck {
 	var cards [60]Card
 	for i := 0; i < 60; i++ {
-		cards[i] = Card{Id: i % 3, Name: ""}
+		cards[i] = Card{Id: i%3 + 1, Name: ""}
 	}
 	return &Deck{
 		Cards: (*[60]Card)(&cards),
