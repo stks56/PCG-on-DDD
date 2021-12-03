24eb38333f95d8eb785e7ce544b8567256b35906
stks56 <t.suenaga428@gmail.com>
2021-12-04 08:25:16 +0900

    Implement field ctrl show

--- app/presentation/controller/fieldController.go
+++ app/presentation/controller/fieldController.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"net/http"
+	"stks56/PCG-on-DDD/app/infrastructure/repository"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,6 +12,11 @@ type FieldController struct{}
 func (fc FieldController) Show() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		fr := repository.NewFieldRepository()
-		return nil
+		field, err := fr.Get()
+		if err != nil {
+			panic("error")
+		}
+
+		return c.JSON(http.StatusOK, field)
 	}
 }
