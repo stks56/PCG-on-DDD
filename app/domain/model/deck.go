cced925951d7baa67b1cd24b12f588d08a3ca09a
stks56 <t.suenaga428@gmail.com>
2021-12-05 08:31:10 +0900

    Fix to pass by slice value in deck cards

--- app/domain/model/deck.go
+++ app/domain/model/deck.go
@@ -5,24 +5,23 @@ import (
 )
 
 type Deck struct {
-	Cards *[60]Card
+	Cards []Card
 }
 
 func NewPresetDeck() *Deck {
-	var cards [60]Card
+	cards := make([]Card, 0, 60)
 	for i := 0; i < 60; i++ {
-		cards[i] = Card{Id: i%3 + 1, Name: ""}
+		cards = append(cards, Card{Id: i%3 + 1, Name: ""})
 	}
 	return &Deck{
-		Cards: (*[60]Card)(&cards),
+		Cards: cards,
 	}
 }
 
 func (deck *Deck) Shuffle(seed int64) {
-	var shuffledCards [60]Card
 	rand.Seed(seed)
 	rand.Shuffle(len(deck.Cards), func(i, j int) {
-		shuffledCards[i], shuffledCards[j] = deck.Cards[j], deck.Cards[i]
+		deck.Cards[i], deck.Cards[j] = deck.Cards[j], deck.Cards[i]
 	})
-	deck.Cards = &shuffledCards
+}
 }
